cb8992478866b9dadfb21b4541559b5f54982a6b
wangna <525100994@qq.com>
2020-01-15 17:05:21 +0800

    Update hostOnline.js

--- vue/config/hostOnline.js
+++ vue/config/hostOnline.js
@@ -3,9 +3,9 @@
  */
 
 export default {
-    domain: 'http://yjj-erp.anlaiye.com',
-    pDomain: 'http://yjj-moles.anlaiye.com',
-    sDomain: 'http://yjj-sns.anlaiye.com',
-    uploadDomain: 'http://manager-center.anlaiye.com',
+//     domain: 'http://yjj-erp.anlaiye.com',
+//     pDomain: 'http://yjj-moles.anlaiye.com',
+//     sDomain: 'http://yjj-sns.anlaiye.com',
+//     uploadDomain: 'http://manager-center.anlaiye.com',
 }
 
