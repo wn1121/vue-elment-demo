b7cd6da43bdd05a2c710591d9deb673369ed1634
wangna <525100994@qq.com>
2020-01-15 17:06:40 +0800

    Update hostTest.js

--- vue/config/hostTest.js
+++ vue/config/hostTest.js
@@ -3,9 +3,9 @@
  */
 
 export default {
-    domain: 'http://yjj-erp-test.imcome.net',
-    pDomain: 'http://yjj-moles-test.imcome.net',
-    sDomain: 'http://yjj-sns-test.imcome.net',
-    uploadDomain: 'http://manager-center.imcoming.com',
+    domain: '',
+    pDomain: '',
+    sDomain: '',
+    uploadDomain: '',
 }
 
